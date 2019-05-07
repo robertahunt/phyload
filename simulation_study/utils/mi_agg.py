ab54bfdb7adbe83643d54500fd405c00da217476
William DeWitt <wsdewitt@gmail.com>
2019-05-06 22:49:33 -0700

    build agg input list with params (closes #16), and use tsv suffixes

--- simulation_study/utils/mi_agg.py
+++ simulation_study/utils/mi_agg.py
@@ -4,7 +4,6 @@
 from matplotlib import pyplot as plt
 import seaborn as sns
 import pandas as pd
-from glob import glob
 
 
 def main():
@@ -14,27 +13,28 @@ def main():
     parser = argparse.ArgumentParser(
                          description='utility script to aggregate mutual '
                                      'information summary stats')
-    parser.add_argument('nest_dir',
+    parser.add_argument('input_list',
                         type=str,
-                        help='simulation nest directory, hierarchically '
-                             'ordered by d, n_iid, n_epi, replicate')
+                        help='path to file listing paths '
+                             'with their parameters')
+    parser.add_argument('outdir',
+                        type=str,
+                        help='output directory')
     args = parser.parse_args()
 
-    paths = glob(f'{args.nest_dir}/*/*/*/*/aln.nex.mi.summary.txt')
-    d, n_iid, n_epi = zip(*[path.split('/')[1:4] for path in paths])
-    d = [float(d) for d in d]
-    n_iid = [int(n_iid) for n_iid in n_iid]
-    n_epi = [int(n_epi) for n_epi in n_epi]
-
-    df_meta = pd.DataFrame({'$d$': d, '$n_i$': n_iid,
-                            '$n_e$': n_epi})
+    df_meta = pd.read_csv(args.input_list, sep='\t')
     df_mi = pd.concat(pd.read_csv(path, sep='\t')
-                      for path in paths).reset_index()
+                      for path in df_meta.path).reset_index()
     # note the groupby and mean below will average over replicates
     df = pd.concat((df_meta, df_mi),
-                   axis=1).groupby(['$d$',
-                                    '$n_i$',
-                                    '$n_e$']).mean().reset_index()
+                   axis=1).groupby(['d',
+                                    'n_iid',
+                                    'n_epi']).mean().reset_index()
+
+    # fancier TeX column names for prettier plots
+    df.rename(index=str,
+              columns={'d': '$d$', 'n_iid': '$n_i$', 'n_epi': '$n_e$'},
+              inplace=True)
 
     def draw_heatmap(*args, **kwargs):
         data = kwargs.pop('data').pivot(index=args[1], columns=args[0],
@@ -45,7 +45,7 @@ def main():
                      square=True, vmin=df.skewness.min(),
                      # vmax=df.skewness.max(), cbar=False, cmap='Reds',
                      )
-    plt.savefig(f'{args.nest_dir}/mi_agg.skewness.pdf')
+    plt.savefig(f'{args.outdir}/mi_agg.skewness.pdf')
 
 
 if __name__ == '__main__':
