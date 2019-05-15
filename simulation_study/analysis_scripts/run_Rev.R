3de36c123595b86b4bcdb3ea33aebd9b2f966a75
Andy Magee <afmagee@uw.edu>
2019-05-15 12:08:42 -0700

    Rev-running script does not need to deal with replicates ever

--- simulation_study/analysis_scripts/run_Rev.R
+++ simulation_study/analysis_scripts/run_Rev.R
@@ -1,22 +1,20 @@
 # This is designed to be called with Rscript with several arguments
-# arg1: number of scripts to make
-# arg2: seed
-# arg3: path to directory to output the revscript
-# arg4: path to rb to call it
+# arg1: seed
+# arg2: path to directory to output the revscript
+# arg3: path to rb to call it
 
 # Call this script from phyload/simulation_study
 
 # Get arguments
 args = commandArgs(trailingOnly=TRUE)
 
-if (!length(args) == 4) {
-  stop("This script requires 4 arguments")
+if (!length(args) == 3) {
+  stop("This script requires 3 arguments")
 }
 
-nscripts  <- args[1]
-this.seed <- args[2]
-out.dir   <- args[3]
-rb.path   <- args[4]
+this.seed <- args[1]
+out.dir   <- args[2]
+rb.path   <- args[3]
 
 
 # Get Rev source script
@@ -25,21 +23,14 @@ revscript <- scan("simulation_scripts/analysis_template.Rev",sep="\n",what="char
 # Make it a length 1 character vector
 revscript <- paste0(revscript,collapse="\n")
 
-# Print revscripts
-for (i in 1:nscripts) {
-  
-  # Fill out the rev template for this simulation cell
-  this.revscript <- revscript
-  this.revscript <- gsub("<<SEED>>",this.seed,this.revscript)
-  this.revscript <- gsub("<<REPLICATE>>",i,this.revscript)
-  
-  cat(this.revscript,file=paste0(out.dir,"/analyze_",i,".Rev"))
-  
-  this.seed <- this.seed + 1
-}
+# Print revscript
 
-# Run revscripts
-for (i in 1:nscripts) {
-  system2(command=as.character(rb.path),args=paste0(out.dir,"/analyze_",i,".Rev"))
-}
+# Fill out the rev template for this simulation cell
+this.revscript <- revscript
+this.revscript <- gsub("<<SEED>>",this.seed,this.revscript)
+
+cat(this.revscript,file=paste0(out.dir,"/analyze.Rev"))
+
+# Run revscript
+system2(command=as.character(rb.path),args=paste0(out.dir,"/analyze.Rev"))
 
