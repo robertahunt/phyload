52cc984d5daf95262cafa66252c9821bad5f789b
Andy Magee <afmagee@uw.edu>
2019-05-15 12:01:15 -0700

    Making analysis revscripts and calling now 1 step, currently runs 1 MCMC generation

--- simulation_study/analysis_scripts/run_Rev.R
+++ simulation_study/analysis_scripts/run_Rev.R
@@ -1,20 +1,23 @@
 # This is designed to be called with Rscript with several arguments
 # arg1: number of scripts to make
 # arg2: seed
-# arg3: path to output alignments (relative to location of phyload repository)
+# arg3: path to directory to output the revscript
+# arg4: path to rb to call it
 
 # Call this script from phyload/simulation_study
 
 # Get arguments
 args = commandArgs(trailingOnly=TRUE)
 
-if (!length(args) == 3) {
-  stop("This script requires 3 arguments")
+if (!length(args) == 4) {
+  stop("This script requires 4 arguments")
 }
 
 nscripts  <- args[1]
 this.seed <- args[2]
-out.dir  <- args[3]
+out.dir   <- args[3]
+rb.path   <- args[4]
+
 
 # Get Rev source script
 revscript <- scan("simulation_scripts/analysis_template.Rev",sep="\n",what="character",strip.white=FALSE)
@@ -22,6 +25,7 @@ revscript <- scan("simulation_scripts/analysis_template.Rev",sep="\n",what="char
 # Make it a length 1 character vector
 revscript <- paste0(revscript,collapse="\n")
 
+# Print revscripts
 for (i in 1:nscripts) {
   
   # Fill out the rev template for this simulation cell
@@ -33,3 +37,9 @@ for (i in 1:nscripts) {
   
   this.seed <- this.seed + 1
 }
+
+# Run revscripts
+for (i in 1:nscripts) {
+  system2(command=as.character(rb.path),args=paste0(out.dir,"/analyze_",i,".Rev"))
+}
+
