e698af9bc3d1b63d7fbcbe3c338300405c6ad487
William DeWitt <wsdewitt@gmail.com>
2019-05-27 00:20:00 -0700

    sbatch script

---
+++ simulation_study/sbatchme.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH
+#SBATCH -o sbatchme.out
+#SBATCH -e sbatchme.err
+#SBATCH -p largenode
+#SBATCH -c 24
+
+revpath=/home/wdewitt/wdewitt_fast/revbayes/projects/cmake/rb
+source activate phyload
+scons --revpath=${revpath} -j 70 -k
