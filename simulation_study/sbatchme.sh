095d6708a57b2f8b4f99a7742dcc402744279c10
William DeWitt <wsdewitt@gmail.com>
2019-06-01 14:29:24 -0700

    no large node?

--- simulation_study/sbatchme.sh
+++ simulation_study/sbatchme.sh
@@ -2,9 +2,9 @@
 #SBATCH
 #SBATCH -o sbatchme.out
 #SBATCH -e sbatchme.err
-#SBATCH -p largenode
-#SBATCH -c 24
-#SBATCH --mem=30000
+##SBATCH -p largenode
+##SBATCH -c 24
+##SBATCH --mem=30000
 
 revpath=/home/wdewitt/wdewitt_fast/revbayes/projects/cmake/rb
 source activate phyload
